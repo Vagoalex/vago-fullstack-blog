91c8d84e88f099bcf365b3d7ed4e1c71753f2636
Александр Ваго <aleksandr.vago@griser.ru>
2022-10-22 18:09:33 +0300

    feat: added remove post

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.get("/auth/me", checkAuth, UserController.getMe);
 app.get("/posts", PostController.getAll);
 app.get("/posts/:id", PostController.getOne);
 app.post("/posts", checkAuth, postCreateValidation, PostController.create);
-// app.delete("/posts/:id", PostController.remove);
+app.delete("/posts/:id", PostController.remove);
 // app.patch("/posts/:id", PostController.update);
 
 app.listen(PORT, (err) => {
